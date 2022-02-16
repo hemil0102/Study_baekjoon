b203f51a97355154658f1cbabe3b751ea99bb0dc
jongmin <asdfaa22@naver.com>
2022-02-16 20:23:30 +0900

    add files

---
+++ JONGMIN/05_1st Array/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  05_1st Array
+//
+//  Created by JONGMIN Youn on 2022/02/16.
+//
+
+import Foundation
+
+print("abcde")
+ß
---
+++ JONGMIN/06_Function/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  06_Function
+//
+//  Created by JONGMIN Youn on 2022/02/16.
+//
+
+import Foundation
+
+print("Hello, World!")
+

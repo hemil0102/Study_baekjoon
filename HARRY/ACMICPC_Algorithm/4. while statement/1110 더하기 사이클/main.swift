ce3827ea1bcec26790b848cec91437e045d206c6
hemil0102 <hemil0102@gmail.com>
2022-02-17 21:45:38 +0900

    17/02/2022

--- HARRY/ACMICPC_Algorithm/4. while statement/1110 더하기 사이클/main.swift
+++ HARRY/ACMICPC_Algorithm/4. while statement/1110 더하기 사이클/main.swift
@@ -11,39 +11,39 @@
 // 
 import Foundation
 
-let input = readLine()!
-var underTenString: String
-var eachNumber = [Character]()
-var newValue: Int = Int(input)!
-var eachNumberNewValue = [Character]()
-var cycle = 0
+let input = readLine()! // String? unwrap
+var underTenString: String // 초기 값을 두 자리로 나누기 위한 변수
+var result = 0 // 계산 결과값
+var newValue = 0 // 새로운 수
+var resultString: String // 결과 값을 두 자리로 나누기 위한 변수
+var initEachNumber = [Character]() // 초기 값을 두 자리로 나누어서 문자 값을 할당할 배열
+var resultEachNumber = [Character]() // 결과 값을 두 자리로 나누어서 문자 값을 할당할 배열
+let underTen = 10 // 입력이 10보다 작은가?
+var initValue1 = 0 // 나눠진 자릿 수를 계산하기 위해 문자를 int로 변환
+var initValue2 = 0 // 나눠진 자릿 수를 계산하기 위해 문자를 int로 변환
+var cycle = 0 // while문 내에서 반복된 값을 증가시킴
 
-if Int(input)! < 10 {
-    underTenString = String(format: "%02d", Int(input)!)
-    print(underTenString)
-    eachNumber = Array(underTenString)
-    print(eachNumber)
+if Int(input)! < underTen {
+    underTenString = String(format: "%02d", Int(input)!) // 10보다 작은 수에 앞자리 0 추가 및 문자열로 변환
+    //print(underTenString) // 앞자리 0 추가되었는지 확인
+    initEachNumber = Array(underTenString) //문자열을 문자 배열로
+    //print(initEachNumber) //잘 할당되었는지 확인
 } else {
-    eachNumber = Array(input)
-    print(eachNumber)
+    initEachNumber = Array(input) //입력이 10보다 클 경우 문자열을 두자리 배열로 할당
+    //print(initEachNumber) //입력이 두자리 문자로 잘 들어갔는지 확인
 }
 
-
 repeat {
-    if let intValue1 = eachNumber[0].wholeNumberValue, let intValue2 = eachNumber[1].wholeNumberValue {
-        newValue = intValue1 + intValue2
-        String(newValue)
-        if let intNewValue1 =
-        cycle += 1
-    }
+    initValue1 = initEachNumber[0].wholeNumberValue!
+    initValue2 = initEachNumber[1].wholeNumberValue!
+    result = initValue1 + initValue2
+    cycle += 1
     
+    resultString = String(format: "%02d", result)
+    resultEachNumber = Array(resultString)
+    initEachNumber[0] = initEachNumber[1]
+    initEachNumber[1] = resultEachNumber[1]
+    newValue = Int(String(initEachNumber))!
+
 } while newValue != Int(input)!
-
-
-    /*
-    let number: Int = 7
-
-            let str = String(format: "%02d", number)
-            
-            print(str) // 07
-     */
+print(cycle)

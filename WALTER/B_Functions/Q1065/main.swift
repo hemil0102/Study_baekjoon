8cc1e4e22e15c47eab3474152b6064c3c024d716
Walter-Jo <ys.mobility@gmail.com>
2022-02-22 13:34:20 +0900

    update quiz

--- WALTER/B_Functions/Q1065/main.swift
+++ WALTER/B_Functions/Q1065/main.swift
@@ -7,3 +7,29 @@
 
 import Foundation
 
+//입력
+let num = Int(readLine()!)!
+
+var result = 0
+
+for n in 1...num {
+    result += getHanNumber(n)
+}
+
+print(result)       //한수 갯수 출력
+
+
+//Han Number
+func getHanNumber(_ num: Int) -> Int {
+    if num < 10 { return 1 }
+
+    var result = 0
+    var nT = num
+    //num 10
+    
+    while nT != 0 {
+        nT /= 10
+    }
+
+    return result
+}
---
+++ WALTER/B_String/B_String/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  B_String
+//
+//  Created by Walter J on 2022/02/22.
+//
+
+import Foundation
+
+print("Hello, World!")
+
